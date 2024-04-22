ddd67071aa1beed9d368e776b2cf0e35ac9df0d4
mehaaamajeed <165750437+mehaaamajeed@users.noreply.github.com>
2024-04-22 17:06:33 +0530

    Add files via upload

---
+++ app/src/assets/data/data.js
@@ -0,0 +1,113 @@
+export const nav = [
+    {
+      id: 1,
+      text: "home",
+      url: "/",
+    },
+    {
+      id: 2,
+      text: "about",
+      url: "/about",
+    },
+    {
+      id: 3,
+      text: "pages",
+      url: "/pages",
+    },
+    {
+      id: 4,
+      text: "blog",
+      url: "/blog",
+    },
+   
+  ]
+  export const category = [
+    {
+      id: 1,
+      category: "INDIAN",
+      title: "Capital Of Food",
+      cover: "/indian.jpg",
+    },
+    {
+      id: 2,
+      category: "ITALIAN",
+      title: "Mamma Mia",
+      cover: "/mamamia.jpg",
+    },
+    {
+      id: 3,
+      category: "MALAYSIAN",
+      title: "Mixture Of Four",
+      cover: "/malaysian.jpg",
+    },
+    {
+      id: 4,
+      category: "FIJIAN",
+      title: "Viti Lovo",
+      cover: "/fijii.jpg",
+    },
+    {
+      id: 5,
+      category: "CARIBBEAN",
+      title: "Beachside Savoury",
+      cover: "/caribbbb.jpg",
+    },
+    
+  ]
+  export const blog = [
+    {
+      id: 1,
+      title: "SUNSHINE FLAVOURS",
+      desc: "The Caribbean is a diverse region known for its stunning beaches, vibrant culture, and rich culinary traditions. With thousands of islands, each offering unique landscapes and cultural influences it a popular destination for tourism and boasts a vibrant music and dance scene. Caribbean cuisine blends African, European, and indigenous flavors, featuring dishes like jerk chicken and rice and peas.",
+      category: "CARIBBEAN",
+      cover: "/carib2.jpg",
+      date: "APRIL 05, 2024",
+    },
+  
+    {
+        id: 2,
+        title: "FIORI FRESCHI",
+        desc: "Italian cuisine is renowned for its simplicity, fresh ingredients, and regional diversity. Pasta, pizza, antipasti, and dolci are iconic staples, each reflecting Italy's rich culinary heritage. With a strong emphasis on quality ingredients and traditional cooking techniques, Italian dishes celebrate the joy of sharing good food with loved ones.",
+        category: "ITALIAN",
+        cover: "/italia.jpg",
+        date: "APRIL 05, 2024",
+      },
+
+      {
+        id: 3,
+        title: "KOKODA ROUROU",
+        desc: "Fijian culture is known for its warmth, hospitality, and rich traditions. The islands boast stunning natural beauty, with pristine beaches and lush rainforests. Traditional customs include elaborate ceremonies, music, dance, and storytelling. Rugby is a major passion for Fijians, reflecting their national pride and unity.",
+        category: "FIJIAN",
+        cover: "/fijian.jpg",
+        date: "APRIL 05, 2024",
+      },
+
+      {
+        id: 4,
+        title: "THE DELICACY AND IMPECCABLE SAVOURY",
+        desc: "Indian cuisine consists of a variety of regional and traditional cuisines native to the Indian subcontinent. Given the diversity in soil, climate, culture, ethnic groups, and occupations, these cuisines vary substantially and use locally available spices, herbs, vegetables, and fruits.Indian food is also heavily influenced by religion, in particular Hinduism and Islam, cultural choices and traditions.",
+        category: "INDIAN",
+        cover: "/masala.jpg",
+        date: "APRIL 05, 2024",
+      },
+
+      {
+        id: 5,
+        title: "MALAYSIAN DELIGHTS",
+        desc: "Malaysian cuisine is a fusion of Malay, Chinese, Indian, and Thai influences, featuring aromatic spices, rice, noodles, seafood, and meats. Signature dishes include nasi lemak, rendang, laksa, satay, and roti canai, reflecting the country's multicultural heritage.",
+        category: "MALAYSIAN",
+        cover: "/malaay.jpg",
+        date: "APRIL 05, 2024",
+      },
+   
+      {
+        id: 3,
+        title: "KOKODA ROUROU",
+        desc: "Fijian culture is known for its warmth, hospitality, and rich traditions. The islands boast stunning natural beauty, with pristine beaches and lush rainforests. Traditional customs include elaborate ceremonies, music, dance, and storytelling. Rugby is a major passion for Fijians, reflecting their national pride and unity.",
+        category: "FIJIAN",
+        cover: "/bar.jpg",
+        date: "APRIL 05, 2024",
+      },   
+    
+  ]
+  
